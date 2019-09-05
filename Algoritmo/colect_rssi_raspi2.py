5a67fc081c7652ec8a3e5fe522b537305aa76396
aldomelladop <aldomellado.1310@gmail.com>
2019-09-05 05:16:00 +0100

    colect_rssi_raspi2.py

--- Algoritmo/colect_rssi_raspi2.py
+++ Algoritmo/colect_rssi_raspi2.py
@@ -41,7 +41,7 @@ try:
                 my_file=open(str(name)+".txt",'a')
                 
 #                t.tic()
-                A  = os.popen('sudo iwlist wlp2s0 scan |egrep "Cell |ESSID|Quality"').readlines()
+                A  = os.popen('sudo iwlist wlan0 scan |egrep "Cell |ESSID|Quality"').readlines()
 #                t.toc('iwlist = ')
 
 #                t.tic()
